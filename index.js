b2b5e9c6c2d5bb59694873db71fd3bb48dcaa5f8
Nick Bottomley <nhbottomley@gmail.com>
2015-01-06 16:24:10 -0800

    initial rewrite

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-/**
- * Created by Ralph Varjabedian on 11/11/14.
- * require-hook is licensed under the [MIT]
- * do not remove this notice.
- */
-
-console.require_hook = {};
-
-console.require_hook.log = function() {
-  console.log.apply(this, ["\033[1;34m[require-hook]\033[0m"].concat(Array.prototype.slice.call(arguments, 0)));
-};
-
-console.require_hook.warn = function() {
-  console.warn.apply(this, ["\033[1;31m[require-hook]\033[0m"].concat(Array.prototype.slice.call(arguments, 0)));
-};
-
-module.exports = require("./lib/requireHook.js");
+module.exports = require("./lib");
--- test/main.test.js
+++ test/main.test.js
@@ -4,51 +4,21 @@
 
 'use strict';
 
-var should = require('should');
-var path = require('path');
+require('chai').should();
 
 describe('testing require-hook', function() {
-  this.timeout(0);
 
-  it('testing require and hook', function(done) {
+  it('testing require and hook', function() {
     var requireHook = require("../");
-    try {
-      requireHook.detach();
-    } catch(err) {
-      should.exist(err);
-    }
+
     var Module = require("module");
     var oldRequire = Module.prototype.require;
+
     Module.prototype.require.should.equal(oldRequire);
-    requireHook.config.testOnlySubPath = []; // remove it, we need the hook to attach to all files in test mode
-    requireHook.config.alternateProjectPaths = ["/anything"];
-    try {
-      requireHook.attach();
-    } catch(err) {
-      should.exist(err);
-    }
+    requireHook.attach();
     Module.prototype.require.should.not.equal(oldRequire);
     requireHook.detach();
-    requireHook.attach();
-    requireHook.getData_localToProject();
-    requireHook.getData_thirdParty();
-    requireHook.getData_localThirdParty();
-    requireHook.getOriginalRequire();
-    done();
-  });
-
-  it('testing setEvent', function(done) {
-    var requireHook = require("../");
-    requireHook.setEvent(function(requireResult, requireCallData){
-    });
-    require("./mathHelper.js");
-    var data = requireHook.getData();
-    should.equal(data.length, 2);
-    should.equal(data[0].getId(), path.join(process.cwd(), "index.js"));
-    should.equal(data[1].getId(), path.join(process.cwd(), "test", "mathHelper.js"));
-    require("./mathHelper.js", "__skip");
-    require("path");
-    done();
+    Module.prototype.require.should.equal(oldRequire);
   });
 
 });
