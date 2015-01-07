2b5353f011c626c7530fbd58e5eb228237881707
Nick Bottomley <nhbottomley@gmail.com>
2015-01-06 18:39:01 -0800

    much improved tests

--- test/index.js
+++ test/index.js
@@ -4,7 +4,11 @@
 
 'use strict';
 
-require('chai').should();
+var should = require('chai').should();
+
+var butt = require('butt');
+
+function noop () {}
 
 describe('replacing Module.prototype.require()', function() {
 
@@ -24,29 +28,82 @@ describe('replacing Module.prototype.require()', function() {
 });
 
 
+
 describe('intercepting require()', function () {
   var requireHook = require("..");
 
-  beforeEach(function () {
-    requireHook.attach();
-  });
-  // afterEach(requireHook.detach);
+  beforeEach(butt(requireHook.attach, 0));
+  afterEach(butt(requireHook.detach, 0));
 
   it('invokes the listener when `require()` is invoked', function () {
-    var result, info;
+    var called = false;
+    function listener () {
+      called = true;
+    }
+    requireHook.setListener(listener);
+    require("./calculator");
+    called.should.equal(true);
+  });
 
+  it('passes the result and some info to the listener', function () {
+    var calculator, result, info;
     function listener (r, i) {
       result = r;
       info = i;
     }
-
     requireHook.setListener(listener);
-
-    var calculator = require("./calculator");
-
+    calculator = require("./calculator");
     calculator.should.equal(result);
-
+    info.should.eql({ 
+      require: './calculator',
+      callingFile: '/Users/nickbottomley/Documents/nb/require-hook/test/index.js',
+      native: false,
+      json: false,
+      absPath: '/Users/nickbottomley/Documents/nb/require-hook/test/calculator.js',
+      absPathResolvedCorrectly: true,
+      testOnly: true,
+      thirdParty: false,
+      localToProject: true 
+    });
   });
 
+  it('allows the listener to pass back a value', function () {
+    function listener () {
+      return true;
+    }
+    requireHook.setListener(listener);
+    var calculator = require("./calculator");
+    calculator.should.equal(true);
+  });
+
+  it('passes an error in the result spot if one occurs', function () {
+    function listener1 (err, info) {
+      (err instanceof Error).should.equal(true);
+      should.exist(info);
+      /unexpected identifier/i.test(err.message).should.equal(true);
+      return true;
+    }
+
+    function listener2 (err, info) {
+      (err instanceof Error).should.equal(true);
+      should.exist(info);
+      /cannot find module/i.test(err.message).should.equal(true);
+      return true;
+    }
+
+    requireHook.setListener(listener1);
+    require("./malformed.js");
+    requireHook.resetListener();
+
+    requireHook.setListener(listener2);
+    require("./no-exist.js");
+  });
+
+  it('throws the error if one is passed back from the listener', function () {
+    requireHook.setListener(noop);
+    (function () { 
+      require("./no-exist.js");
+    }).should.throw(/cannot find module/i);
+  });
 
 });
---
+++ test/malformed.js
@@ -0,0 +1,4 @@
+module.exports = {
+  a: 0
+  b: 1
+};
