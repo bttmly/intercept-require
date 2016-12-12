e18e1de9630af2be788979ad5ef32244c7b1c28d
Nick Bottomley <nhbottomley@gmail.com>
2016-12-11 20:49:20 -0800

    DRY up the tests a bit

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ const path = require("path");
 const assert = require("assert");
 const Module = require("module");
 const __require = Module.prototype.require;
-
 const expect = require("expect");
 
 const intercept = require("../lib/index");
@@ -12,50 +11,13 @@ const intercept = require("../lib/index");
 const CANNOT_FIND = /cannot find module/i;
 const UNEXPECTED_IDENTIFIER = /unexpected identifier/i;
 
-function escapeRegExp(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-}
-
-function validateObj (target, validator) {
-  Object.keys(validator).forEach(function (key) {
-    if (!target.hasOwnProperty(key)) {
-      throw new Error("Target object doesn't have key " + key);
-    }
-
-    if (validator[key] instanceof RegExp) {
-      if (!validator[key].test(target[key])) {
-        const msg = [
-          "Target object's",
-          key,
-          "property didn't match RegExp",
-          "\n",
-          target[key]
-        ].join(" ");
-        throw new Error(msg);
-      }
-      return;
-    }
-
-    if (validator[key] !== target[key]) {
-      const msg = [
-        "Expected properties at key",
-        key,
-        "to be equal.",
-        "\n",
-        "Actual: ",
-        target[key],
-        "Expected: ",
-        validator[key]
-      ].join(" ");
-      throw new Error(msg);
-    }
-  });
-}
-
 describe("intercept-require", function () {
+
+  let restore;
 
   // ensure we're back in a sane state after each test;
   afterEach(function () {
+    if (restore) restore();
     assert(Module.prototype.require === __require, "Failed to call restore()")
   });
 
@@ -70,63 +32,51 @@ describe("intercept-require", function () {
     });
   });
 
-  function checkCalculator (calc) {
-    return ["add", "subtract", "multiply", "divide"]
-      .every(calc.hasOwnProperty.bind(calc));
-  }
-
   describe("intercepting require()", function () {
     const intercept = require("../lib/index");
 
     it("invokes the listener when `require()` is invoked", function () {
       let called = false;
-      const restore = intercept(() => {
-        called = true
-      });
+      restore = intercept(() => { called = true });
       const calc = require("./calculator.js");
       expect(checkCalculator(calc)).toEqual(true);
       expect(called).toEqual(true);
-      restore();
     });
 
     it("passes the result and some info to the listener", function () {
-      let calc, result, info;
+
+      let result, info;
       function listener (r, i) {
         result = r;
         info = i;
       }
 
-      const restore = intercept(listener);
-      calc = require("./calculator");
+      restore = intercept(listener);
+      const calc = require("./calculator");
       expect(calc).toEqual(result);
       expect(checkCalculator(calc)).toEqual(true);
 
       const absPath = path.join(__dirname, "../test/calculator.js");
       const callingFile = path.join(__dirname, "../test/test.js");
 
-      validateObj(info, {
-        callingFile: callingFile,
-        moduleId: "./calculator",
-        extname: ".js",
-        native: false,
-        thirdParty: false,
-        absPath: absPath,
-        absPathResolvedCorrectly: true,
-        local: true,
-      });
-
-      restore();
+      expect(info.callingFile).toBe(callingFile);
+      expect(info.moduleId).toBe("./calculator");
+      expect(info.extname).toBe(".js");
+      expect(info.native).toBe(false);
+      expect(info.thirdParty).toBe(false);
+      expect(info.absPath).toBe(absPath);
+      expect(info.absPathResolvedCorrectly).toBe(true);
+      expect(info.local).toBe(true);
     });
 
     it("allows the listener to pass back a value", function () {
-      const restore = intercept(function () { return true; });
+      restore = intercept(function () { return true; });
       const calculator = require("./calculator.js");
       expect(calculator).toEqual(true);
-      restore();
     });
 
     it("passes an as `info.error` if one occurs, and result and `null`", function () {
-      let wasCalled1, wasCalled2, restore;
+      let wasCalled1, wasCalled2;
 
       function listener1 (result, info) {
         expect(result).toNotExist();
@@ -150,39 +100,24 @@ describe("intercept-require", function () {
       restore = intercept(listener2);
       require("./no-exist.js");
       expect(wasCalled2).toBe(true);
-
-      restore();
     });
 
     it("when no listener is attached, `require` should act normally", function () {
-      expect(function () {
-        require("./no-exist.js");
-      }).toThrow(CANNOT_FIND);
-
-      expect(function () {
-        require("./malformed");
-      }).toThrow(UNEXPECTED_IDENTIFIER);
-
+      expect(() => require("./no-exist.js")).toThrow(CANNOT_FIND);
+      expect(() => require("./malformed")).toThrow(UNEXPECTED_IDENTIFIER);
       const calc = require("./calculator");
       expect(Object.keys(calc).sort()).toEqual(["add", "divide", "multiply", "subtract"]);
     });
 
     it("setListener throws when passed anything but a function", function () {
-      expect(function () {
-        intercept()
-      }).toThrow(/argument must be a function/)
+      expect(intercept).toThrow(/argument must be a function/)
     });
 
     it("allows short circuiting", function () {
-      const restore = intercept(function () {
-        return result;
-      }, {
-        shortCircuit: true
-      });
+      restore = intercept(() => result, {shortCircuit: true});
       const result = {works: true};
       const exported = require("./no-exist");
       expect(exported).toBe(result);
-      restore();
     });
 
     it("allows short circuiting with matching", function () {
@@ -193,7 +128,7 @@ describe("intercept-require", function () {
       let timesShortCircuitedSucceeded = 0;
       let timesShortCircuitSkipped = 0;
 
-      const restore = intercept(function (original, info) {
+      restore = intercept(function (original, info) {
         if (info.didShortCircuit) {
           timesShortCircuitedSucceeded++;
           return shortCircuitResult;
@@ -211,20 +146,20 @@ describe("intercept-require", function () {
       const didntShortCircuit = require("./calculator");
 
       expect(didShortCircuit).toBe(shortCircuitResult);
-      expect(trueRequireResult).toBe(didntShortCircuit);
+      expect(didntShortCircuit).toBe(trueRequireResult);
       expect(timesShortCircuitSkipped).toBe(1);
       expect(timesShortCircuitedSucceeded).toBe(1);
-
-      restore();
     });
 
     it("should not throw on native modules", function () {
-      const restore = intercept(() => {});
-      expect(function() {
-        require("fs");
-      }).toNotThrow();
-      restore();
+      restore = intercept(() => {});
+      expect(() => require("fs")).toNotThrow();
     });
 
   });
 });
+
+function checkCalculator (calc) {
+  return ["add", "subtract", "multiply", "divide"]
+    .every(calc.hasOwnProperty.bind(calc));
+}
